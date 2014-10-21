0481ddb4ffaae831d6189c8f10d1d822130e63c5
Art Mills <art@hamcois.com>
2014-10-20 21:51:44 -0500

    Up to 38.

---
+++ ex33.rb
@@ -0,0 +1,19 @@
+def whiling(number)
+i = 0
+numbers = []
+
+while i < number
+	puts "At the top i is #{i}"
+	numbers.push(i)
+
+	i += 1
+	puts "Numbers now: ", numbers
+	puts "At the bottom i is #{i}"
+end
+
+puts "The numbers: "
+numbers.each {|num| puts num}
+
+end
+
+whiling(10)
